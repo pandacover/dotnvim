66924d7478e15dc671b698b1c4abfa378330ff44
pandacover <luvmakin01@gmail.com>
2025-03-28 11:16:25 +0530

    chore(nvim): nvim config

---
+++ init.lua
@@ -0,0 +1,23 @@
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+require 'config.opts'
+require 'config.keymaps'
+
+require 'config.lazy'
+
+-- Setup lazy.nvim
+require('lazy').setup {
+  spec = {
+    -- import your plugins
+    { import = 'plugins' },
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { 'habamax' } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+}
